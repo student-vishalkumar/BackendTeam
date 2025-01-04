c76b201da26c8f26512be61b14f85fd2ed8cfb7d
student-vishalkumar <vishalbjrkumar@gmail.com>
2025-01-04 16:27:03 +0530

    database config done(dev_env)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 
+import connectDB from './config/dbConfig.js';
 import { PORT } from './config/serverConfig.js';
 
 const app = express();
@@ -13,4 +14,10 @@ app.get('/ping', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`server is running on ${PORT}`);
+    connectDB()
 })
+
+// kumar878vishal
+// oE2mCd5P34Nv1fEO
+
+// mongodb+srv://kumar878vishal:<db_password>@cluster0.xb6rw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
